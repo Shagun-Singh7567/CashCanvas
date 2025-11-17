9d286fcdb6b7f1dbebc6995cd3acf6020e197111
Shagun Singh <shagundun@gmail.com>
2025-11-17 15:31:29 +0530

    feat: Completed transaction module

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,26 @@
-
+import services.transactions as tr
 def main():
-    ch = int(input("Press 1 to see transaction history, 2 for charts menu, and 3 to convert currency: "))
+    ch = int(input("Press 1 for transactions, 2 for charts menu, 3 to convert currency and 4 to exit: "))
+    
     if(ch == 1):
-        print("Let's review your transaction history")
-    # TODO: Code for transaction menu
+        c1 = int(input("Press 1 to display transaction history, 2 to make a new transaction, and 3 to exit"))
+        if(c1 == 1):
+            tr.displayhistory()
+        if(c1 == 2):
+            tr.makeTransaction()
+        if(c1 == 3):
+            return 0
+
     elif(ch == 2):
         print("Charts menu")
     # TODO: Code for matplotlib
     elif(ch == 3):
         print("Currency convertor")
     # TODO: Currency conversion code
+
+    elif(ch == 4):
+        return 0
+    
     else:
         print("Wrong choice, please try again!")
     
---
+++ src/services/transactions.py
@@ -0,0 +1,31 @@
+from datetime import date
+import csv
+
+data = []
+
+def displayhistory(): # Displaying transaction history
+    print("Transactions made: ")
+    with open("src/services/Transactions.csv", newline="") as csvfile:
+        reader = csv.reader(csvfile) # Here, the reader variable stores the contents of the csv file
+        next(reader)
+        for row in csvfile:
+            print(row)
+
+def makeTransaction(): # Function to make a transaction
+    amt = int(input("Enter the amount withdrawn: "))
+    current_date = date.today()
+    print(current_date)
+    reason = input("Enter the purpose of making the transaction: ")
+    category = input("Enter the category of the transaction: ")
+    data = [amt, current_date, reason, category]
+    print(data)
+
+    storeTransaction(data)
+
+def storeTransaction(data): # Function to store the data in the list to a csv
+    with open("src/services/Transactions.csv", "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(data)
+
+
+    
